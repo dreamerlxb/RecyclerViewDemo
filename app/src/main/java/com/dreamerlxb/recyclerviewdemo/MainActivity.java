0d8720ed749b15a699d1734eea0510a40561119f
lxb <424151385@qq.com>
2017-06-09 11:36:02 +0800

    =======

--- app/src/main/java/com/dreamerlxb/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/dreamerlxb/recyclerviewdemo/MainActivity.java
@@ -1,5 +1,9 @@
 package com.dreamerlxb.recyclerviewdemo;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -28,6 +32,31 @@ public class MainActivity extends AppCompatActivity implements MainAdapter.Norma
         MainAdapter adapter = new MainAdapter(this, MyData.getNormalData());
         adapter.setItemClickListener(this);
         recyclerView.setAdapter(adapter);
+
+
+        // 本地通知
+        NotificationManager nm=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String title = "通知标题" ;
+        String content = "通知内容" ;
+
+        Intent intent = new Intent(this, HeaderAndFooterActivity.class);
+        PendingIntent pi= PendingIntent.getActivity(this, 0, intent, 0);
+
+        //1.实例化一个通知，指定图标、概要、时间
+        //2.指定通知的标题、内容和intent
+        Notification n = new Notification.Builder(this)
+                .setContentTitle(title)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText(content)
+                .setWhen(5000)
+                .setContentIntent(pi)
+                .build();
+
+        //3.指定声音
+        n.defaults = Notification.DEFAULT_SOUND;
+
+        //4.发送通知
+        nm.notify(1, n);
     }
 
     @Override
